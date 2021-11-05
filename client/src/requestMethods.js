d4edb9ee24ece0fa30abf1bff847873f164f4c51
ser veresta <subramaniang573@gmail.com>
2021-11-05 20:59:07 +0530

    Deploy

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://ecommerce-1000.herokuapp.com/";
 const token =
   (localStorage.getItem("persist:root") &&
     JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.token) ||
